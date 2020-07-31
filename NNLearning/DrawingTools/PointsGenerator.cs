4da97e89f86e91be471948ba09c2a1acaead93e8
Amine <aminefrioua@gmail.com>
2020-07-31 19:29:45 +0900

    Drawing Circles Yey

---
+++ NNLearning/DrawingTools/PointsGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Shapes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace NNLearning.DrawingTools
+{
+    public class PointsGenerator
+    {
+        public Point[] Points { get; set; }
+
+        public Ellipse[] Ellipses { get; set; }
+
+        private readonly Random _myRandomizer;
+
+        public PointsGenerator(int numOfPoints, int canvasWidth, int canvasHeight)
+        {
+            _myRandomizer = new Random();
+
+            Points = new Point[numOfPoints];
+
+            Ellipses = new Ellipse[numOfPoints];
+
+            for (int i = 0; i < numOfPoints; i++)
+            {
+                Points[i] = GeneratePoint(canvasWidth, canvasHeight);
+                Ellipses[i] = new Ellipse()
+                {
+
+                    Width = 10,
+                    Height = 10,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 2,
+                    Fill = Brushes.Blue
+                };
+            }
+        }
+
+        private Point GeneratePoint(int width, int height)
+        {
+            return new Point(_myRandomizer.Next(0, width), _myRandomizer.Next(0, height));
+        }
+
+        public void DrawCircles(Canvas canvas)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                canvas.Children.Add(Ellipses[i]);
+
+                Ellipses[i].SetValue(Canvas.LeftProperty, Points[i].X);
+
+               Ellipses[i].SetValue(Canvas.TopProperty, Points[i].Y);
+            }
+        }
+
+    }
+}
--- NNLearning/MainWindow.xaml.cs
+++ NNLearning/MainWindow.xaml.cs
@@ -1,28 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NNLearning.DrawingTools;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NNLearning
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        public PointsGenerator PointsGenerator { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            PointsGenerator = new PointsGenerator(100, (int)Window.Width, (int)Window.Height);
+
+            PointsGenerator.DrawCircles(Screen);
+
+
         }
+
+
     }
 }
